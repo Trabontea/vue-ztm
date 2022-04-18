7827fc56bef49dce6e1ac9a867ef67fd3a832a7c
Trabontea <danielmbotea@gmail.com>
2022-04-19 00:02:49 +0300

    directives

--- section-2-1-STARTER/app.js
+++ section-2-1-STARTER/app.js
@@ -1,2 +1,40 @@
 // Mount applications
-Vue.createApp({}).mount('#app');
+//vm view model
+const vm = Vue.createApp({
+  data() {
+    return {
+      firstName: "Jhon",
+      lastName: "Doe",
+      url: "https://google.com",
+      raw_url: '<a href="https://google.com" target="blank">Google</a>',
+      age: 20,
+    };
+  },
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    },
+    increment() {
+      this.age++;
+    },
+    updateLastName(msg, event) {
+      event.preventDefault();
+      this.lastName = event.target.value;
+      console.log(msg);
+    },
+  },
+}).mount("#app");
+
+// setTimeout(() => {
+//   vm.firstName = "Bob";
+// }, 2000);
+
+// Mount applications
+// Vue.createApp({
+//   data() {
+//     return {
+//       firstName: "Jhonel",
+//       lastName: "pula",
+//     };
+//   },
+// }).mount("#app2");
